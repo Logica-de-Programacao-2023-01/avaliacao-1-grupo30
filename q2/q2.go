4fbdef20fac06a191f8efcbe5f4f294eb938bf08
kalefi <127106411+kalefi@users.noreply.github.com>
2023-04-24 11:18:25 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -12,7 +12,21 @@ package q2
 //
 //Ajude os amigos a encontrar o número de problemas para os quais eles escreverão uma solução.
 
-func ProblemsSolved(answers [][3]bool) int {
-	// Seu código aqui
-	return 0
+package q2
+
+func ProblelmSolved(answers [][3]bool) int { 2 usages
+  count:=0
+  for i := 0; 1<len(answers); i++ {
+   soma := 0
+   for j := 0; j < 3; j++ {
+   if answers[i][j] {
+   soma++
+   }
+   }
+   if soma >= 2 {
+   count++
+   }
+   }
+   return count
+ 
 }
